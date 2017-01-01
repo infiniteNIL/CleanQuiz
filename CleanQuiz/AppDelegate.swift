2d21b25e3d1de3a81cc608c456a1837c977e756c
Rod Schmidt <rod@infinitenil.com>
2017-01-01 14:42:33 -0700

    Initial project

---
+++ CleanQuiz/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  CleanQuiz
+//
+//  Created by Rod Schmidt on 1/1/17.
+//  Copyright © 2017 infiniteNIL. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+

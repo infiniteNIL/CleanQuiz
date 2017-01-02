c52ab43caaf778e27c465a4aeec7726df23e7692
Rod Schmidt <rod@infinitenil.com>
2017-01-01 20:48:39 -0700

    Add FlashCard entity

---
+++ CleanQuiz/Common/FlashCard.swift
@@ -0,0 +1,15 @@
+//
+//  FlashCard.swift
+//  CleanQuiz
+//
+//  Created by Rod Schmidt on 1/1/17.
+//  Copyright © 2017 infiniteNIL. All rights reserved.
+//
+
+import Foundation
+
+struct FlashCard
+{
+    let question: String
+    let answer: String
+}
